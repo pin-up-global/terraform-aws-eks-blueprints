1fc214fbfaa8c899e279acbe0d9659aa467dbeec
Volodymyr Roshko <v.roshko@pin-up.tech>
2024-07-05 14:59:39 +0700

    test

--- data.tf
+++ data.tf
@@ -9,7 +9,8 @@ data "aws_region" "current" {}
 data "aws_eks_cluster" "cluster" {
   count = var.create_eks ? 1 : 0
   #name  = module.aws_eks.cluster_id
-  name  = module.aws_eks.cluster_name
+  #name  = module.aws_eks.cluster_name
+  name = var.cluster_name
 }
 
 data "http" "eks_cluster_readiness" {
