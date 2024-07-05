18963c77d34f6677f467939e786415ba015dac6d
Volodymyr Roshko <v.roshko@pin-up.tech>
2024-07-05 16:55:46 +0700

    test

--- data.tf
+++ data.tf
@@ -6,15 +6,19 @@ data "aws_region" "current" {}
 #   count = var.create_eks ? 1 : 0
 #   name  = var.cluster_name
 # }
+# data "aws_eks_cluster" "cluster" {
+#   count = var.create_eks ? 1 : 0
+#   #name  = module.aws_eks.cluster_id
+#   name  = module.aws_eks.cluster_name
+#   #name = try(module.aws_eks.cluster_id, module.aws_eks.cluster_name)
+# }
+
 data "aws_eks_cluster" "cluster" {
   count = var.create_eks ? 1 : 0
-  #name  = module.aws_eks.cluster_id
-  name  = module.aws_eks.cluster_name
-  #name = try(module.aws_eks.cluster_id, module.aws_eks.cluster_name)
-
-#   depends_on = [
-#     data.http.eks_cluster_readiness[0]
-#   ]
+  name = var.cluster_name
+  depends_on = [
+    module.aws_eks.cluster_name
+  ]
 }
 
 data "http" "eks_cluster_readiness" {
