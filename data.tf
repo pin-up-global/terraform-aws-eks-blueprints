e3840c3dc17e3f9ea24ef3d0e2c23eca6e88a53d
Volodymyr Roshko <v.roshko@pin-up.tech>
2024-07-05 16:48:31 +0700

    test

--- data.tf
+++ data.tf
@@ -12,9 +12,9 @@ data "aws_eks_cluster" "cluster" {
   name  = module.aws_eks.cluster_name
   #name = try(module.aws_eks.cluster_id, module.aws_eks.cluster_name)
 
-  depends_on = [
-    data.http.eks_cluster_readiness[0]
-  ]
+#   depends_on = [
+#     data.http.eks_cluster_readiness[0]
+#   ]
 }
 
 data "http" "eks_cluster_readiness" {
